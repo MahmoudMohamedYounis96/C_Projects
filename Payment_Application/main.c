1b953b52fa4e88551c7ac34fc6f1d76618750ded
Mahmoud_Mohamed <108106732+MahmoudMohamedYounis96@users.noreply.github.com>
2022-08-07 08:44:23 +0200

    Create the project

---
+++ Payment_Application/main.c
@@ -0,0 +1,30 @@
+/* 
+ * main.c
+ * 
+ *	@Created on: Aug 6, 2022
+ * 
+ *		@Author: Mahmoud Mohamed Youness.
+ *			>> Wireless Embedded System Softwear Engineer.
+ *			>> Communication Team, EL-Sewedy Electrometer Group.
+ */
+// ======================== Includes =============================
+// >> Standard input/output libraries.
+#include <stdio.h>
+#include <stdlib.h>
+
+// >> Standard integer types library. 
+#include <stdint.h>
+
+// >> Standard string library.
+#include <string.h>
+
+// >> Standard mathematical library.
+#include <math.h>
+
+// >> Standard character types library.
+#include <ctype.h>
+
+// ========================= Main function ========================
+int main(void) {
+	return 0;
+}
